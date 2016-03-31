b3598da0674cf9b869e9e53237cb38c00e5ff71f
Netvon <tomvannimwegen@gmail.com>
2016-03-31 13:49:47 +0200

    Updated Auth pages to Arrow style

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,108 +1,106 @@
-@extends('layouts.app', ['page_title' => trans('auth.register')])
+@extends('layouts.arrow_page', ['page_title' => 'Register'])
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">{{ trans('auth.register') }}</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
-                        {!! csrf_field() !!}
+@section('page_content')
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+            <div class="panel-heading">{{ trans('auth.register') }}</div>
+            <div class="panel-body">
+                <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+                    {!! csrf_field() !!}
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">{{ trans('user.name') }}</label>
+                    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                        <label class="col-md-4 control-label">{{ trans('user.name') }}</label>
 
-                            <div class="col-md-6">
-                                <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
 
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @if ($errors->has('name'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('name') }}</strong>
+                                </span>
+                            @endif
                         </div>
+                    </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">{{ trans('user.email') }}</label>
+                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <label class="col-md-4 control-label">{{ trans('user.email') }}</label>
 
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                        <div class="col-md-6">
+                            <input type="email" class="form-control" name="email" value="{{ old('email') }}">
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @if ($errors->has('email'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
                         </div>
+                    </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">{{ trans('user.password') }}</label>
+                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <label class="col-md-4 control-label">{{ trans('user.password') }}</label>
 
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
+                        <div class="col-md-6">
+                            <input type="password" class="form-control" name="password">
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @if ($errors->has('password'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
                         </div>
+                    </div>
 
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">{{ trans('user.confirm_password') }}</label>
+                    <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                        <label class="col-md-4 control-label">{{ trans('user.confirm_password') }}</label>
 
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password_confirmation">
+                        <div class="col-md-6">
+                            <input type="password" class="form-control" name="password_confirmation">
 
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @if ($errors->has('password_confirmation'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                </span>
+                            @endif
                         </div>
+                    </div>
 
-                        <div class="form-group{{ $errors->has('date_of_birth') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">{{ trans('user.date_of_birth') }}</label>
+                    <div class="form-group{{ $errors->has('date_of_birth') ? ' has-error' : '' }}">
+                        <label class="col-md-4 control-label">{{ trans('user.date_of_birth') }}</label>
 
-                            <div class="col-md-6">
-                                <input type="date" class="form-control" name="date_of_birth">
+                        <div class="col-md-6">
+                            <input type="date" class="form-control" name="date_of_birth">
 
-                                @if ($errors->has('date_of_birth'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('date_of_birth') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @if ($errors->has('date_of_birth'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('date_of_birth') }}</strong>
+                                </span>
+                            @endif
                         </div>
+                    </div>
 
-                        <div class="form-group{{ $errors->has('phone_number') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">{{ trans('user.phone_number') }}</label>
+                    <div class="form-group{{ $errors->has('phone_number') ? ' has-error' : '' }}">
+                        <label class="col-md-4 control-label">{{ trans('user.phone_number') }}</label>
 
-                            <div class="col-md-6">
-                                <input type="tel" class="form-control" name="phone_number">
+                        <div class="col-md-6">
+                            <input type="tel" class="form-control" name="phone_number">
 
-                                @if ($errors->has('phone_number'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('phone_number') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @if ($errors->has('phone_number'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('phone_number') }}</strong>
+                                </span>
+                            @endif
                         </div>
+                    </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-user"></i>Register
-                                </button>
-                            </div>
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fa fa-btn fa-user"></i>Register
+                            </button>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
