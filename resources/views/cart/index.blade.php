4c6f6d8d34f057debbbe6bb23251075b01e72748
Netvon <tomvannimwegen@gmail.com>
2016-03-31 16:29:05 +0200

    Updated cart to include delete and change quantity functionality

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -10,64 +10,79 @@
 
             <div class="box">
 
-                {!! Form::open() !!}
-
-                    <div class="table-responsive">
-                        <table class="table">
-                            <thead>
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th colspan="2">Product</th>
+                            <th>Quantity</th>
+                            <th>Unit price</th>
+                            <th colspan="2">Total</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($cart as $pq)
                             <tr>
-                                <th colspan="2">Product</th>
-                                <th>Quantity</th>
-                                <th>Unit price</th>
-                                <th colspan="2">Total</th>
+                                <td>
+                                    <a href="{{ URL::action('ProductController@show', ['products' => $pq['product']->slug]) }}">
+                                        <img alt="{{ $pq['product']->name }}">
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="{{ URL::action('ProductController@show', ['products' => $pq['product']->slug]) }}">
+                                        {{ $pq['product']->name }}
+                                    </a>
+                                </td>
+                                <td>
+                                    <div class="form-control">{{ $pq['quantity'] }}</div>
+                                    {!! Form::open(['action' => ['CartController@store'], 'class' => 'form-inline']) !!}
+                                    {!! Form::hidden('product_id', $pq['product']->id) !!}
+                                    {!! Form::hidden('quantity', $pq['quantity'] + 1) !!}
+                                    <button type="submit"><i class="fa fa-plus"></i></button>
+                                    {!! Form::close() !!}
+                                    @if($pq['quantity'] > 1)
+                                        {!! Form::open(['action' => ['CartController@store'], 'class' => 'form-inline']) !!}
+                                        {!! Form::hidden('product_id', $pq['product']->id) !!}
+                                        {!! Form::hidden('quantity', $pq['quantity'] - 1) !!}
+                                        <button type="submit"><i class="fa fa-minus"></i></button>
+                                        {!! Form::close() !!}
+                                    @endif
+                                </td>
+                                <td>{{ '$'.$pq['product']->price }}</td>
+                                <td>{{ '$'.($pq['product']->price * $pq['quantity'])}}</td>
+                                <td>
+                                    {!! Form::open(['method' => 'DELETE', 'action' => ['CartController@destroy'], 'class' => 'form-inline']) !!}
+                                    {!! Form::hidden('product_id', $pq['product']->id) !!}
+                                    <button type="submit"><i class="fa fa-trash-o"></i></button>
+                                    {!! Form::close() !!}
+                                </td>
                             </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($cart as $pq)
-                                <tr>
-                                    <td>
-                                        <a href="{{ URL::action('ProductController@show', ['products' => $pq['product']->slug]) }}">
-                                            <img  alt="{{ $pq['product']->name }}">
-                                        </a>
-                                    </td>
-                                    <td><a href="{{ URL::action('ProductController@show', ['products' => $pq['product']->slug]) }}">
-                                            {{ $pq['product']->name }}
-                                        </a>
-                                    </td>
-                                    <td>
-                                        <input type="number" value="{{ $pq['quantity'] }}" class="form-control">
-                                    </td>
-                                    <td>{{ '$'.$pq['product']->price }}</td>
-                                    <td>{{ '$'.($pq['product']->price * $pq['quantity'])}}</td>
-                                    <td><a href="#"><i class="fa fa-trash-o"></i></a>
-                                    </td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                            <tfoot>
-                            <tr>
-                                <th colspan="5">Total</th>
-                                <th colspan="2">----</th>
-                            </tr>
-                            </tfoot>
-                        </table>
+                        @endforeach
+                        </tbody>
+                        <tfoot>
+                        <tr>
+                            <th colspan="5">Total</th>
+                            <th colspan="2">----</th>
+                        </tr>
+                        </tfoot>
+                    </table>
 
+                </div>
+                <!-- /.table-responsive -->
+
+                <div class="box-footer">
+                    <div class="pull-left">
+                        <a href="{{ URL::action('CategoryController@index') }}" class="btn btn-default"><i
+                                    class="fa fa-chevron-left"></i>
+                            Continue shopping</a>
                     </div>
-                    <!-- /.table-responsive -->
-
-                    <div class="box-footer">
-                        <div class="pull-left">
-                            <a href="{{ URL::action('CategoryController@index') }}" class="btn btn-default"><i class="fa fa-chevron-left"></i>
-                                Continue shopping</a>
-                        </div>
-                        <div class="pull-right">
-                            <a href="{{ URL::action('CartController@order') }}" class="btn btn-template-main">Proceed to checkout <i
-                                        class="fa fa-chevron-right"></i>
-                            </a>
-                        </div>
+                    <div class="pull-right">
+                        <a href="{{ URL::action('CartController@order') }}" class="btn btn-template-main">Proceed to
+                            checkout <i
+                                    class="fa fa-chevron-right"></i>
+                        </a>
                     </div>
-
-                {!! Form::close() !!}
+                </div>
 
             </div>
     @else
