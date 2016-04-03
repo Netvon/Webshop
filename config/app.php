e120fbb4d90124f7ce9223238de2589b15fe99dc
Netvon <tomvannimwegen@gmail.com>
2016-04-03 21:54:29 +0200

    Added Markdown support for blogs

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
 
 
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        GrahamCampbell\Markdown\MarkdownServiceProvider::class,
     ],
 
     /*
@@ -207,7 +208,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-
+        'Markdown' => GrahamCampbell\Markdown\Facades\Markdown::class,
     ],
 
 ];
